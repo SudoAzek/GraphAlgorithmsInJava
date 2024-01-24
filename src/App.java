f4385968a10a21c589147a225294301cfa961cd7
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2024-01-23 22:42:35 -0500

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,39 @@
+
+
+public class App {
+	private static int [][] adjacencyMatrix = {
+		{0, 2, 4, 0},
+		{0, 0, 0, 3},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0}};
+
+	public static void main(String[] args) {
+
+		// find all edges
+		for (int i = 0; i < adjacencyMatrix.length; i++) {
+			for (int j = 0; j < adjacencyMatrix.length; j++) {
+				System.out.println("Edge with weight: " + adjacencyMatrix[i][j]);
+			}
+		}
+	}
+
+	System.out.println(adjacencyMatrix[0][2]);
+
+	// Adjacency list representation
+	
+	Vertex a = new Vertex("A");
+	Vertex b = new Vertex("B");
+	Vertex c = new Vertex("C");
+	Vertex d = new Vertex("D");
+
+	a.addNeighbor(b);
+	b.addNeighbor(c);
+	c.addNeighbor(d);
+
+	// Print all neighbors
+	a.showNeighbors();
+	b.showNeighbors();
+	c.showNeighbors();
+	d.showNeighbors();
+ }
+}
